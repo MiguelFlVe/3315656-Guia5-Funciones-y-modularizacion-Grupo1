7f5ad3a0ef6d5d783ae0d8f03c009647024c1c12
jesus david <jesusdavidbecerralopez69@gmail.com>
2026-03-25 10:50:04 -0500

    feat(apropiacion):realice el ejercicio4 era Diseñar un diagrama de flujo que simule un cajero automático

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import { registrarIngresoEstudiantes } from "./Apropiación/Ejercicio 2.js";
 //Ejercicio 3
 import { numerosMayoresA50 } from "./Apropiación/Ejercicio 3.js";
 // Ejercicio 7
+ */
+import { simularCajero } from "./Apropiación/Ejercicio 4.js";
 import { sumatoria } from "./Apropiación/Ejercicio 7.js"
 // Ejercicio 8
 import { Adults } from "./Apropiación/Ejercicio 8.js"
@@ -23,93 +25,98 @@ import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
 // Menú de selección de parte de la guía y ejercicio a ejecutar
-while (true){
-    // Elección de parte de la guía a ejecutar
-    let choose = prompt("¿Qué parte de la guía deseas ejecutar (Apropiación [A] o Transferencia [T])? (Escriba solo la letra inicial del nombre de sección, en mayúscula): ");
+while (true) {
+  // Elección de parte de la guía a ejecutar
+  let choose = prompt("¿Qué parte de la guía deseas ejecutar (Apropiación [A] o Transferencia [T])? (Escriba solo la letra inicial del nombre de sección, en mayúscula): ");
 
-    // Se selecciona la parte de Apropiación
-    if (choose === "A") {
-        // Elección del ejercicio a ejecutar dentro de Apropiación
-        let ejercicio = prompt("¿Qué ejercicio de apropiación deseas ejecutar (1-8)?: ");
-        
-        // Se selecciona el ejercicio 1
-        if (ejercicio === "1") {
-            console.log("Ejercicio 1 de Apropiación:");
-            console.log("Diseña un código que muestre los números del 1 al 20, pero solo imprima los múltiplos de 3.");
-            console.log(multiplosDeTres());
-        }
+  // Se selecciona la parte de Apropiación
+  if (choose === "A") {
+    // Elección del ejercicio a ejecutar dentro de Apropiación
+    let ejercicio = prompt("¿Qué ejercicio de apropiación deseas ejecutar (1-8)?: ");
 
-        //Se selecciona el ejercicio 2
-        else if (ejercicio === "2") {
-            console.log("Ejercicio 2 de Apropiación:");
-            console.log("Registrar ingreso de 5 estudiantes, validando carné.");
-            console.log(registrarIngresoEstudiantes(prompt));
-        }
-        //Se selecciona el ejercicio 3
-        else if (ejercicio === "3") {
-            console.log("Ejercicio 3 de Apropiación:");
-            console.log("Realiza un diagrama de flujo que pida al usuario ingresar 10 números y muestre únicamente los que sean mayores que 50.");
-            console.log(numerosMayoresA50(prompt));
-        }
+    // Se selecciona el ejercicio 1
+    if (ejercicio === "1") {
+      console.log("Ejercicio 1 de Apropiación:");
+      console.log("Diseña un código que muestre los números del 1 al 20, pero solo imprima los múltiplos de 3.");
+      console.log(multiplosDeTres());
+    }
+
+    //Se selecciona el ejercicio 2
+    else if (ejercicio === "2") {
+      console.log("Ejercicio 2 de Apropiación:");
+      console.log("Registrar ingreso de 5 estudiantes, validando carné.");
+      console.log(registrarIngresoEstudiantes(prompt));
+    }
+    //Se selecciona el ejercicio 3
+    else if (ejercicio === "3") {
+      console.log("Ejercicio 3 de Apropiación:");
+      console.log("Realiza un diagrama de flujo que pida al usuario ingresar 10 números y muestre únicamente los que sean mayores que 50.");
+      console.log(numerosMayoresA50(prompt));
+    }
+    //Se selecciona el ejercicio 4
+    else if (ejercicio === "4") {
+      console.log("Ejercicio 4 de Apropiación:");
+      console.log("4. Diseña un diagrama de flujo que simule un cajero automático: ");
+      console.log(numerosMayoresA50(prompt));
 
         // Se selecciona el ejercicio 7
         else if (ejercicio === "7") {
-            console.log("Ejercicio 7 de Apropiación:");
-            console.log("Diseña un código que sume los números del 1 al 100, pero interrumpa el ciclo si encuentra un número mayor a 90 que sea par.")
-            console.log(sumatoria());
-        }
-        
-        // Se selecciona el ejercicio 8
-        else if (ejercicio === "8") {
-            console.log("Ejercicio 8 de Apropiación:");
-            console.log("Crea un diagrama de flujo que lea el nombre y la edad de 5 personas. Solo mostrará en pantalla aquellas personas que tengan 18 años o más.");
-            console.log(Adults());
-        }
-        
-        // Se selecciona un ejercicio no válido
-        else {
-            console.log("Ejercicio no válido. Por favor, elige un número entre 1 y 8.");
-        }
+        console.log("Ejercicio 7 de Apropiación:");
+        console.log("Diseña un código que sume los números del 1 al 100, pero interrumpa el ciclo si encuentra un número mayor a 90 que sea par.")
+        console.log(sumatoria());
+      }
+
+      // Se selecciona el ejercicio 8
+      else if (ejercicio === "8") {
+        console.log("Ejercicio 8 de Apropiación:");
+        console.log("Crea un diagrama de flujo que lea el nombre y la edad de 5 personas. Solo mostrará en pantalla aquellas personas que tengan 18 años o más.");
+        console.log(Adults());
+      }
+
+      // Se selecciona un ejercicio no válido
+      else {
+        console.log("Ejercicio no válido. Por favor, elige un número entre 1 y 8.");
+      }
     }
-    
+
     // Se selecciona la parte de Transferencia
     else if (choose === "T") {
-        let ejercicio = prompt("¿Qué ejercicio de transferencia deseas ejecutar (1-8)?: ");
+      let ejercicio = prompt("¿Qué ejercicio de transferencia deseas ejecutar (1-8)?: ");
 
-        if (ejercicio === "1") {
-            console.log("Ejercicio 1 de Transferencia:");
-            console.log("Crea un programa que: muestre todos los números primos entre 1 y 50.");
-            console.log(numerosPrimos());
-        }
+      if (ejercicio === "1") {
+        console.log("Ejercicio 1 de Transferencia:");
+        console.log("Crea un programa que: muestre todos los números primos entre 1 y 50.");
+        console.log(numerosPrimos());
+      }
 
-        else if (ejercicio === "7") {
-            console.log("Ejercicio 7 de Transferencia:");
-            console.log("Crea un programa que: permita al usuario ingresar 10 números. Al final, muestra cuántos fueron pares y cuántos impares.");
-            console.log(evenodd());
-        }
-        
-        else if (ejercicio === "8") {
-            console.log("Ejercicio 8 de Transferencia:");
-            console.log("Crea un programa que: solicite un usuario y contraseña. El programa debe permitir hasta 3 intentos; si el usuario falla en todos, mostrar un mensaje de “Acceso denegado”. Si acierta, mostrar “Bienvenido”.");
-            console.log(login());
-        }
+      else if (ejercicio === "7") {
+        console.log("Ejercicio 7 de Transferencia:");
+        console.log("Crea un programa que: permita al usuario ingresar 10 números. Al final, muestra cuántos fueron pares y cuántos impares.");
+        console.log(evenodd());
+      }
 
-        else {
-            console.log("Ejercicio no válido. Por favor, elige un número entre 1 y 8.");
-        }
+      else if (ejercicio === "8") {
+        console.log("Ejercicio 8 de Transferencia:");
+        console.log("Crea un programa que: solicite un usuario y contraseña. El programa debe permitir hasta 3 intentos; si el usuario falla en todos, mostrar un mensaje de “Acceso denegado”. Si acierta, mostrar “Bienvenido”.");
+        console.log(login());
+      }
+
+      else {
+        console.log("Ejercicio no válido. Por favor, elige un número entre 1 y 8.");
+      }
     }
-    
+
     // Se selecciona una sección no válida
     else {
-        console.log("Opción no válida. Por favor, elige 'A' para Apropiación o 'T' para Transferencia.");
+      console.log("Opción no válida. Por favor, elige 'A' para Apropiación o 'T' para Transferencia.");
     }
 
     // Se cuestiona al usuario sobre su deseo de ejecutar otro Ejercicio
     let continueChoice = prompt("¿Deseas ejecutar otro ejercicio? (S/N): ");
-    
+
     // En caso de que el usuario no desea continuar explícitamente, se cierra el programa
     if (continueChoice.toUpperCase() !== "S") {
-        console.log("¡Gracias por usar el programa! Hasta luego.");
-        break;
+      console.log("¡Gracias por usar el programa! Hasta luego.");
+      break;
     }
-}
+  }
